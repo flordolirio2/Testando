efdf6f838d014e29b2d4e26228cf9a5a9adfb26a
Suzane <Suzane@asus-ssd>
2015-11-10 12:09:34 -0300

    outravez

--- src/com/flordolirio2/Teste/Teste.java
+++ src/com/flordolirio2/Teste/Teste.java
@@ -5,6 +5,8 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("testando1");
 		System.out.println("Agora é minha vez");
+		System.out.println("naosei");
+		
 
 	}
 
